d913b8984f0d522867733c085f512f65f8b38aa5
karelVR <karel.vanroey@cegeka.com>
2019-08-22 19:04:34 +0200

    If applied, this commit will add the bootstrapped base application created by Spring Initializr + the maven build plugin, as seems to be required by Heroku

---
+++ src/main/java/be/amazingpeople/sandbox22082019/Sandbox22082019Application.java
@@ -0,0 +1,13 @@
+package be.amazingpeople.sandbox22082019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sandbox22082019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sandbox22082019Application.class, args);
+	}
+
+}
---
+++ src/test/java/be/amazingpeople/sandbox22082019/Sandbox22082019ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.amazingpeople.sandbox22082019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sandbox22082019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ab0c113f1079e73587edc7e97ca49d48b8159e5a
karelVR <karel.vanroey@cegeka.com>
2019-08-23 08:03:54 +0200

    Adding a non-functional sqlite

--- src/main/java/be/amazingpeople/sandbox22082019/Sandbox22082019Application.java
+++ src/main/java/be/amazingpeople/sandbox22082019/Sandbox22082019Application.java
@@ -1,10 +1,16 @@
 package be.amazingpeople.sandbox22082019;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class Sandbox22082019Application {
+
+    @Autowired
+	JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sandbox22082019Application.class, args);
--- src/main/java/be/amazingpeople/sandbox22082019/SandboxHello.java
+++ src/main/java/be/amazingpeople/sandbox22082019/SandboxHello.java
@@ -1,14 +1,24 @@
 package be.amazingpeople.sandbox22082019;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SandboxHello {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/")
     @ResponseBody
     String beKind() {
+        jdbcTemplate.execute("DROP TABLE IF EXISTS sandbox");
+        jdbcTemplate.execute("CREATE TABLE sandbox (id integer , name string)");
+
+        jdbcTemplate.execute("insert into sandbox values(1, 'sandbox 20190822')");
+
         return "Hello everybody @ Sandbox 22/08/2019 \uD83C\uDF55 \uD83C\uDF55 \uD83C\uDF55";
     }
 }
